7a1111903f26f3ec436059410d092f2ccf6a663f
bishal udash <supertramp.bishal@gmail.com>
2021-03-13 18:37:07 +0545

    image and margin issues fixed

--- src/utils/RichText.js
+++ src/utils/RichText.js
@@ -1,31 +1,49 @@
-import React from 'react'
+import React from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 
-const  modules = {
-    toolbar: [
-      [{ 'header': [1, 2, false] }],
-      ['bold', 'italic', 'underline','strike', 'blockquote'],
-      [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
-      [{ 'align': [] }],
-      [{ 'color': [] }, { 'background': [] }],
-      ['link', 'image', 'video'],
-      ['blockquote', 'code-block'],
-      ['clean']
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, false] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" },
     ],
-  }
+    [{ align: [] }],
+    [{ color: [] }, { background: [] }],
+    ["link", "image", "video"],
+    ["blockquote", "code-block"],
+    ["clean"],
+  ],
+  clipboard: {
+    matchVisual: false,
+  },
+};
 
-const  formats = [
-    'header',
-    'bold', 'italic', 'underline', 'strike', 'blockquote',
-    'list', 'bullet', 'indent',
-    'align',
-    'color','background',
-    'link', 'image','video',
-    'blockquote', 'code-block',
-  ];
+const formats = [
+  "header",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "list",
+  "bullet",
+  "indent",
+  "align",
+  "color",
+  "background",
+  "link",
+  "image",
+  "video",
+  "blockquote",
+  "code-block",
+];
 
-const RichText = ({value, setValue}) => {
+const RichText = ({ value, setValue }) => {
   return (
     <div>
       <ReactQuill
@@ -34,13 +52,12 @@ const RichText = ({value, setValue}) => {
         name="content"
         modules={modules}
         formats={formats}
-        onChange={(e) =>{
-          setValue({ ...value, content: e })
-        }
-        }
+        onChange={(e) => {
+          setValue({ ...value, content: e });
+        }}
       />
     </div>
-  )
-}
+  );
+};
 
-export default RichText
+export default RichText;
